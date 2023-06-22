c76dc6e072a0a4f5686dab4bec14206ca34d5193
Gabriel <gabrielrengifo211@gmail.com>
2023-06-22 06:58:21 -0400

    first steps to make the youtube api work

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+//AIzaSyB4xWqoPFrjOOm3CI24jz1TdHMEZiVO8CA
+//upload playlist id: PLU6MwRDOX1vWVuF1UneZYWVmep7kAamML
+
+//GET https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=PLU6MwRDOX1vWVuF1UneZYWVmep7kAamML&key=AIzaSyB4xWqoPFrjOOm3CI24jz1TdHMEZiVO8CA
+
+const ytVideos = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=PLU6MwRDOX1vWVuF1UneZYWVmep7kAamML&key=AIzaSyB4xWqoPFrjOOm3CI24jz1TdHMEZiVO8CA';
+
+
 app.set('view engine','ejs');
 app.use(express.static('public'))
 
@@ -13,7 +21,13 @@ app.get('/',(req,res) => {
     res.status(200).render('homepage', { "Content-Type":"application/json"});
 })
 
-app.get('/classes',(req,res) => {
+app.get('/classes',async (req,res) => {
+
+    let response = await fetch(ytVideos)
+    .then(list => list.json());
+
+    console.log(response)
+
     res.status(200).render('classes');
 })
 
